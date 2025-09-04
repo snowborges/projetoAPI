4d5e77c694339d242e6177f691ff23532f0dc489
letícia <122499469+hellticia@users.noreply.github.com>
2025-09-04 15:51:15 -0300

    finalizando

--- src/servidor.js
+++ src/servidor.js
@@ -2,13 +2,29 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 })
-    console.log('Middleware 1...')
-    next()
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`) 
 })
+
